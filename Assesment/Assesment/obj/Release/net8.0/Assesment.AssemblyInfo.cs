e27699c743107cfcf16c34299f7ccf992494e032
Longjie Luo <Longjie.Luo@Solera.com>
2025-10-01 03:56:00 +0800

    QAQ

--- Assesment/Assesment/obj/Release/net8.0/Assesment.AssemblyInfo.cs
+++ Assesment/Assesment/obj/Release/net8.0/Assesment.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assesment")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cbe8a36ae1c7acf71f9aca7d9d2366fa805ec30")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assesment")]
