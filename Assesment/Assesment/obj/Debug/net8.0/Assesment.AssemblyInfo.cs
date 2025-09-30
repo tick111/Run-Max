a7a02d7ca0968f4db052baa0dc9e495bbc675630
Longjie Luo <Longjie.Luo@Solera.com>
2025-10-01 05:10:14 +0800

    Assement

--- Assesment/Assesment/obj/Debug/net8.0/Assesment.AssemblyInfo.cs
+++ Assesment/Assesment/obj/Debug/net8.0/Assesment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assesment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e27699c743107cfcf16c34299f7ccf992494e032")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6405cd762079b255eae4ccba4a6a97a68ddafb85")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assesment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assesment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
